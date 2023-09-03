d7866eb06d0053f3faa5c3b2d78aae55e3f7e048
yhl48 <limjohnyh@gmail.com>
2023-09-03 23:22:28 +0000

    added algorithm options and debugging code

--- deer/experiments/deer_rnn/train.py
+++ deer/experiments/deer_rnn/train.py
@@ -2,7 +2,6 @@ from typing import Tuple, Dict, Any, Optional, List
 import os
 from functools import partial
 import argparse
-import pickle
 from tqdm import tqdm
 import jax
 import jax.numpy as jnp
@@ -11,20 +10,16 @@ import matplotlib.pyplot as plt
 from tensorboardX import SummaryWriter
 from flax import linen as nn
 import optax
-import numpy as np
-import scipy.integrate
-from deer.seq1d import solve_ivp, seq1d
-from PIL import Image
-from dataloaders.lra_image import CIFAR10DataModule
-from dataloaders.lra_pathfinderx import PathfinderXDataModule
-import torch
 import pdb
-from utils import prep_batch, count_params, get_datamodule
-from models import StackedGRU, MLP
+from utils import prep_batch, count_params, get_datamodule, compute_metrics, grad_norm
+from models import StackedGRU, MLP, MultiScaleGRU
+from deer.seq1d import seq1d
+import sys
 # # run on cpu
 # jax.config.update('jax_platform_name', 'cpu')
 # enable float 64
 jax.config.update('jax_enable_x64', True)
+# jax.config.update("jax_debug_nans", True)
 
 
 @partial(jax.jit, static_argnames=("model", "mlp", "method"))
@@ -37,7 +32,6 @@ def rollout(
     inputs: jnp.ndarray,
     yinit_guess: Optional[jnp.ndarray] = None,
     method: str = "deer_rnn",
-    nclasses: int = 10
 ) -> jnp.ndarray:
     # roll out the model's predictions with y being the state
     # y0: (nstates,)
@@ -46,28 +40,30 @@ def rollout(
     # returns: (ntpts, nstates)
 
     def model_func(carry, inputs, params):
-        # return model.apply(params, carry, inputs)
+        # params is a dictionary here
+        # return jax.lax.scan(partial(model.apply, params), carry, inputs)[1]
         return model.apply(params, carry, inputs)[0]
+
     if method == "deer_rnn":
-        y = seq1d(model_func, y0, inputs, params, yinit_guess)  # (nbatch, nseq, nstates)
+        # y = model_func(y0, inputs, params)
+        # return mlp.apply(mlp_params, y[-1, :]), y
+        y = seq1d(model_func, y0, inputs, params, yinit_guess)  # (nseq, nstates) in a vmap
+        # jax.debug.print("{finalhidden}", finalhidden=y[-1, :])
+        # return y[-1, :], y
+
+        # return mlp.apply(mlp_params, y[-1, :]), y
+        # take the mean along the sequence length before feeding into a dense layer
+        return mlp.apply(mlp_params, jnp.mean(y, axis=0)), y
+    if method == "deer_rnn_last":
+        y = seq1d(model_func, y0, inputs, params, yinit_guess)  # (nseq, nstates) in a vmap
+        # jax.debug.print("{finalhidden}", finalhidden=y[-1, :5])
+        # jax.debug.print("{firsthidden}", firsthidden=y[1, :5])
         return mlp.apply(mlp_params, y[-1, :]), y
+    elif method == "rnn":
+        y = jax.lax.scan(partial(model.apply, params), y0, inputs)[1]
+        return mlp.apply(mlp_params, jnp.mean(y, axis=0)), y
     else:
         raise NotImplementedError()
-
-
-def compute_metrics(
-    logits: jnp.ndarray,
-    labels: jnp.ndarray
-) -> Dict[str, jnp.ndarray]:
-    loss = jnp.mean(optax.softmax_cross_entropy_with_integer_labels(logits, labels))
-    # print_argmax = jax.tree_map(print, jnp.argmax(logits, -1))
-    # jax.debug.print("{classes} {labels}", classes=jnp.argmax(logits, -1), labels=labels)
-    accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
-    metrics = {
-        'loss': loss,
-        'accuracy': accuracy
-    }
-    return metrics
 
 
 @partial(jax.jit, static_argnames=("model", "mlp", "method"))
@@ -82,7 +78,8 @@ def loss_fn(
     method: str = "deer_rnn"
 ) -> jnp.ndarray:
     """
-    batch: (x, y). x.shape == (nbatch, nseq, ndim), y.shape == (nbatch,)
+    y0 (nbatch, nstates)
+    batch (nbatch, nseq, ndim) (nbatch,)
     """
     # compute the loss
     # batch: (batch_size, ntpts, ninp), (batch_size, ntpts, nstates)
@@ -93,12 +90,18 @@ def loss_fn(
     # TODO
     # y0 and yinit_guess
     yinit_guess = all_yinit_guess
-    # y0 = all_yinit_guess[:, 0, :]
+    y0 = all_yinit_guess[:, 0, :]  # I checked the code, think this line is important
 
     # ypred: (batch_size, nclass)
     ypred, yinit_guess = jax.vmap(
-        rollout, in_axes=(None, None, None, None, 0, 0, 0)
-    )(model, params, mlp, mlp_params, y0, x, yinit_guess)
+        rollout, in_axes=(None, None, None, None, 0, 0, 0, None)
+    )(model, params, mlp, mlp_params, y0, x, yinit_guess, method)
+    # ypred = mlp.apply(mlp_params, ypred)
+    # jax.debug.print("{finalhidden}", finalhidden=yinit_guess[:, -1, :])
+    # jax.debug.print("{ypred}", ypred=ypred)
+    # jax.debug.print("{yshape}", yshape=yinit_guess.shape)
+    # print(ypred.shape)
+    # pdb.set_trace()
 
     metrics = compute_metrics(ypred, y)
     loss, accuracy = metrics["loss"], metrics["accuracy"]
@@ -117,15 +120,19 @@ def update_step(
     all_yinit_guess: jnp.ndarray,
     method: str = "deer_rnn"
 ) -> Tuple[optax.Params, Any, jnp.ndarray, jnp.ndarray]:
+    """
+    y0 (nbatch, nstates)
+    batch (nbatch, nseq, ndim) (nbatch,)
+    """
     (loss, (accuracy, yinit_guess)), grad = jax.value_and_grad(
         loss_fn,
         argnums=(1, 3),
         has_aux=True
     )(model, combined_params["params"], mlp, combined_params["mlp_params"], y0, batch, all_yinit_guess, method)
-    # jax.debug.print("{params} {mlp_params}", params=grad[0], mlp_params=grad[1])
     updates, opt_state = optimizer.update({"params": grad[0], "mlp_params": grad[1]}, opt_state)
     combined_params = optax.apply_updates(combined_params, updates)
-    return combined_params, opt_state, loss, accuracy, yinit_guess
+    gradnorm = grad_norm(grad[0])
+    return combined_params, opt_state, loss, accuracy, yinit_guess, gradnorm
 
 
 def main():
@@ -137,10 +144,12 @@ def main():
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--version", type=int, default=0)
     parser.add_argument("--method", type=str, default="deer_rnn")
+    parser.add_argument("--ninps", type=int, default=1)
     parser.add_argument("--nstates", type=int, default=256)
     parser.add_argument("--nsequence", type=int, default=1024)
     parser.add_argument("--nclass", type=int, default=10)
     parser.add_argument("--nlayer", type=int, default=5)
+    parser.add_argument("--ngru", type=int, default=10)
     parser.add_argument(
         "--dset", type=str, default="pathfinder32",
         choices=[
@@ -151,16 +160,21 @@ def main():
             "cifar10",
             "cifar10grayscale",
             "listops",
-            "aan"
+            "aan",
+            "sanity_check",
+            "eigenworms",
+            "ecg200"
         ],
     )
     args = parser.parse_args()
 
     method = args.method
+    ninps = args.ninps
     nstates = args.nstates
     nsequence = args.nsequence
     nclass = args.nclass
     nlayer = args.nlayer
+    ngru = args.ngru
     dtype = jnp.float64
 
     # check the path
@@ -178,18 +192,20 @@ def main():
     #     subkey1,
     #     (args.batch_size, nstates)
     # )  # (batch_size, nstates)
-    model = StackedGRU(nlayer=nlayer, nhidden=nstates, dtype=dtype)
+    # model = StackedGRU(nlayer=nlayer, nhidden=nstates, dtype=dtype)
+    model = MultiScaleGRU(ngru=ngru, nlayer=nlayer, nhidden=nstates, dtype=dtype)
     carry = model.initialize_carry(
         args.batch_size
     )  # (batch_size, nstates)
     inputs = jax.random.normal(
         subkey2,
-        (args.batch_size, nsequence, 1),
+        (args.batch_size, nsequence, ninps),
         dtype=dtype
     )  # (batch_size, nsequence, nstates)
     params = model.init(key, carry, inputs[:, 0, :])
+    # params = model.init_params(key, carry, inputs[:, 0, :])
 
-    mlp = MLP(nclass, dtype)
+    mlp = MLP(nclass, nstates, dtype)
     dummy_x = jax.random.normal(
         subkey3,
         (args.batch_size, nstates),
@@ -197,7 +213,11 @@ def main():
     )  # (batch_size, nstates)
     mlp_params = mlp.init(subkey4, dummy_x)
 
-    optimizer = optax.adam(learning_rate=args.lr)
+    # optimizer = optax.adam(learning_rate=args.lr)
+    optimizer = optax.chain(
+        optax.clip_by_global_norm(max_norm=.1),
+        optax.adam(learning_rate=args.lr)
+    )
     combined_params = {"params": params, "mlp_params": mlp_params}
     opt_state = optimizer.init(combined_params)
 
@@ -206,6 +226,8 @@ def main():
     print(count1, count2)
 
     yinit_guess = jnp.zeros((args.batch_size, nsequence, nstates), dtype=dtype)
+    # yinit_guess = jax.random.normal(subkey4, (args.batch_size, nsequence, nstates), dtype=dtype)
+    # y0 = yinit_guess[:, 0, :]
     y0 = jnp.zeros((args.batch_size, nstates), dtype=dtype)
     # y0 = [jnp.zeros((args.batch_size, nstates), dtype=dtype) for _ in range(nlayer)]
 
@@ -216,33 +238,37 @@ def main():
     step = 0
     dm = get_datamodule(dset=args.dset, batch_size=args.batch_size)
     dm.setup()
-    for epoch in tqdm(range(args.nepochs)):
-        for i, batch in tqdm(enumerate(dm.train_dataloader())):
-            if i > 0:
-                break
+    for epoch in tqdm(range(args.nepochs), file=sys.stderr):
+        loop = tqdm(dm.train_dataloader(), total=len(dm.train_dataloader()), leave=False, file=sys.stderr)
+        for i, batch in enumerate(loop):
+            # if i > 0:
+            #     break
             try:
                 batch = dm.on_before_batch_transfer(batch, i)
             except:
                 pass
             batch = prep_batch(batch, dtype)
-            combined_params, opt_state, loss, accuracy, yinit_guess = update_step(
+            combined_params, opt_state, loss, accuracy, yinit_guess, gradnorm = update_step(
                 model, mlp, optimizer, combined_params,
                 opt_state, batch,
-                y0, yinit_guess
+                y0, yinit_guess, method
             )
+            # y0 = yinit_guess[:, 0, :]
             summary_writer.add_scalar("train_loss", loss, step)
             summary_writer.add_scalar("train_accuracy", accuracy, step)
+            summary_writer.add_scalar("gru_gradnorm", gradnorm, step)
             step += 1
 
         val_loss = 0
         nval = 0
         val_acc = 0
-        for i, batch in tqdm(enumerate(dm.val_dataloader())):
-            if i > 0:
-                break
+        loop = tqdm(dm.val_dataloader(), total=len(dm.val_dataloader()), leave=False, file=sys.stderr)
+        for i, batch in enumerate(loop):
+            # if i > 0:
+            #     break
             batch = dm.on_before_batch_transfer(batch, i)
             batch = prep_batch(batch, dtype)
-            loss, (accuracy, yinit_guess) = loss_fn(
+            loss, (accuracy, _) = loss_fn(
                 model, combined_params["params"], mlp,
                 combined_params["mlp_params"], y0, batch,
                 yinit_guess
