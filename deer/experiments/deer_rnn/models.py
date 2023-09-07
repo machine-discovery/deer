3499fa2b170c94e5f56c7df77a71dd57791e9b79
yhl48 <limjohnyh@gmail.com>
2023-09-07 18:48:54 +0000

    working MultiScaleGRU with equinox

--- deer/experiments/deer_rnn/models.py
+++ deer/experiments/deer_rnn/models.py
@@ -1,8 +1,15 @@
-from typing import Any
+import functools
+from typing import Any, List, Tuple, Callable, Sequence
+
 import jax
 import jax.numpy as jnp
-from jax.nn.initializers import glorot_uniform, xavier_uniform
+# from jax.nn.initializers import glorot_uniform, xavier_uniform
+import equinox as eqx
 from flax import linen as nn
+from jax._src import prng
+
+from deer.seq1d import seq1d
+
 import pdb
 
 
@@ -10,19 +17,6 @@ def he_uniform(key, shape, dtype):
     fan_in = shape[-1]
     bound = jnp.sqrt(6 / fan_in)
     return jax.random.uniform(key, shape, dtype, minval=-bound, maxval=bound)
-
-
-class MLP(nn.Module):
-    nout: int
-    nstates: int
-    dtype: Any
-
-    @nn.compact
-    def __call__(self, x):
-        x = nn.Dense(self.nstates, dtype=self.dtype, kernel_init=he_uniform)(x)
-        x = nn.tanh(x)
-        x = nn.Dense(self.nout, dtype=self.dtype, kernel_init=he_uniform)(x)
-        return x
 
 
 class TmpScaleGRU(nn.Module):
@@ -59,6 +53,19 @@ class TmpScaleGRU(nn.Module):
         return states, states
 
 
+# class MLP(nn.Module):
+#     nstates: int
+#     nout: int
+#     dtype: Any
+
+#     @nn.compact
+#     def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+#         x = nn.Dense(self.nstates, dtype=self.dtype, kernel_init=he_uniform)(x)
+#         x = nn.tanh(x)
+#         x = nn.Dense(self.nout, dtype=self.dtype, kernel_init=he_uniform)(x)
+#         return x
+
+
 # class ScaleGRU(nn.Module):
 #     nstates: int
 #     scale: float
@@ -77,44 +84,318 @@ class TmpScaleGRU(nn.Module):
 #         )
 #         self.log_s = self.param("log_s", self.scaled_initializers(self.scale), ())
 
-
 #     @nn.compact
-#     def __call__(self, h0: jnp.ndarray, inputs: jnp.ndarray):
+#     def __call__(self, h0: jnp.ndarray, inputs: jnp.ndarray):        
 #         # h0.shape == (nbatch, nstates)
 #         # inputs.shape == (nbatch, ninp)
-#         log_s = 1
-#         s = jnp.exp(log_s)
+#         s = jnp.exp(self.log_s)
 #         states, _ = self.gru(h0, inputs / s)
-#         # pdb.set_trace()
 #         states = (states - h0) / s + h0
-#         # pdb.set_trace()
 #         return states, states
 
 
-# class MLP(nn.Module):
-#     nstates: int
-#     nout: int
-#     dtype: Any
+# def MultiScaleGRU(
+#     nchannels: int,
+#     nstates: int,
+#     nlayers: int,
+#     nclasses: int,
+#     dtype: Any,
+# ):
+#     assert nstates % nchannels == 0
+#     gru_nstates = int(nstates / nchannels)
 
-#     @nn.compact
-#     def __call__(self, x):
-#         x = nn.Dense(self.nstates, dtype=self.dtype, kernel_init=he_uniform)(x)
-#         x = nn.tanh(x)
-#         x = nn.Dense(self.nout, dtype=self.dtype, kernel_init=he_uniform)(x)
-#         return x
+#     # encode inputs (or rather, project) to have nstates in the feature dimension
+#     encoder = MLP(nstates=nstates, nout=nstates, dtype=dtype)
+
+#     # nlayers of (scale_gru + mlp) pair
+#     scale_grus = [[
+#         ScaleGRU(
+#             nstates=gru_nstates,
+#             scale=10 ** i,
+#             dtype=dtype
+#         ) for i in range(nchannels)] for _ in range(nlayers)
+#     ]
+#     mlps = [
+#         MLP(nstates=nstates, nout=nstates, dtype=dtype) for i in range(nlayers)
+#     ]
+#     assert len(scale_grus) == nlayers
+#     assert len(scale_grus[0]) == nchannels
+#     assert len(mlps) == nlayers
+
+#     # project nstates in the feature dimension to nclasses for classification
+#     classifier = MLP(nstates=nstates, nout=nclasses, dtype=dtype)
+
+#     return
 
 
 # class MultiScaleGRU(nn.Module):
-#     nchannel: int
+#     nchannels: int
 #     nstates: int
+#     nlayers: int
+#     nclasses: int
 #     dtype: Any
-    
+
 #     def initialize_carry(self, batch_size):
-#         return jnp.ones((batch_size, self.nhidden))
+#         assert self.nstates % self.nchannels == 0
+#         gru_nstates = int(self.nstates / self.nchannels)
+
+#         return jnp.ones((batch_size, gru_nstates))
 
 #     def setup(self):
-#         self.encoder = MLP(nstates=self.nstates, dtype=self.dtype)
-#         self.scale_grus = 
-#         self.mlps = [
-            
+#         assert self.nstates % self.nchannels == 0
+#         gru_nstates = int(self.nstates / self.nchannels)
+
+#         # encode inputs (or rather, project) to have nstates in the feature dimension
+#         self.encoder = MLP(nstates=self.nstates, nout=self.nstates, dtype=self.dtype)
+
+#         # nlayers of (scale_gru + mlp) pair
+#         self.scale_grus = [[
+#             ScaleGRU(
+#                 nstates=gru_nstates,
+#                 scale=10 ** i,
+#                 dtype=self.dtype
+#             ) for i in range(self.nchannels)] for _ in range(self.nlayers)
 #         ]
+#         self.mlps = [
+#             MLP(nstates=self.nstates, nout=self.nstates, dtype=self.dtype) for i in range(self.nlayers)
+#         ]
+#         assert len(self.scale_grus) == self.nlayers
+#         assert len(self.scale_grus[0]) == self.nchannels
+#         assert len(self.mlps) == self.nlayers
+
+#         # project nstates in the feature dimension to nclasses for classification
+#         self.classifier = MLP(nstates=self.nstates, nout=self.nclasses, dtype=self.dtype)
+
+#     def __call__(self, h0: List[jnp.ndarray], inputs: jnp.ndarray, yinit_guess: List[jnp.ndarray]):
+#         # assert len(h0) == self.nchannels
+
+#         # encode (or rather, project) the inputs
+#         inputs = self.encoder(inputs)
+
+#         for i in range(self.nlayers):
+#             x_from_all_channels = []
+#             for ch in range(self.nchannels):
+#                 print(i, ch)
+#                 print(inputs.shape)
+#                 pdb.set_trace()
+
+#                 # def model_func(carry: jnp.ndarray, inputs: jnp.ndarray, params: Any):
+#                 #     return self.scale_grus[i][ch](carry, inputs)
+#                 def model_func(carry: jnp.ndarray, inputs: jnp.ndarray, params: Any):
+#                     return self.scale_grus[i][ch].apply({"params": params[f"scaled_grus_{i}"]}, carry, inputs)
+
+#                 # x = seq1d(
+#                 #     func=model_func,
+#                 #     y0=h0,
+#                 #     xinp=inputs,
+#                 #     params=0,
+#                 #     yinit_guess=yinit_guess  # yinit_guess[i]
+#                 # )
+#                 # x = jax.vmap(seq1d, in_axes=(None, 0, 0, None, 0))(
+#                 #     model_func,
+#                 #     h0,
+#                 #     inputs,
+#                 #     None,
+#                 #     yinit_guess  # yinit_guess[i]
+#                 # )
+#                 x = nn.vmap(seq1d, in_axes=(0, 0, None, 0))(
+#                     self.scale_grus[i][ch],
+#                     h0,
+#                     inputs,
+#                     None,
+#                     yinit_guess  # yinit_guess[i]
+#                 )
+#                 pdb.set_trace()
+#                 x_from_all_channels.append(x)
+#             x = jnp.concatenate(x_from_all_channels, axis=-1)
+#             x = self.mlp[i](x)
+#             inputs = x
+#         return self.mlp[-1](x)
+
+
+def vmap_to_shape(func: Callable, shape: Sequence[int]):
+    rank = len(shape)
+    for i in range(rank - 1):
+        func = jax.vmap(func)
+    return func
+
+
+class MLP(eqx.Module):
+    model: eqx.nn.Sequential
+
+    def __init__(self, ninp: int, nstate: int, nout: int, key: prng.PRNGKeyArray):
+        self.model = eqx.nn.MLP(
+            in_size=ninp,
+            out_size=nout,
+            width_size=nstate,
+            depth=1,
+            activation=jax.nn.tanh,
+            key=key
+        )
+
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        return vmap_to_shape(self.model, x.shape)(x)
+
+
+class ScaleGRU(eqx.Module):
+    # check if everything defined here is meant to be trainable
+    gru: eqx.Module
+    log_s: jnp.ndarray  # check if this is trainable
+
+    def __init__(self, ninp: int, nstate: int, scale: float, key: prng.PRNGKeyArray):
+        self.gru = eqx.nn.GRUCell(
+            input_size=ninp,
+            hidden_size=nstate,
+            key=key
+        )
+        self.log_s = jnp.log(jnp.ones(1) * scale)
+
+    def __call__(self, inputs: jnp.ndarray, h0: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        # h0.shape == (nbatch, nstate)
+        # inputs.shape == (nbatch, ninp)
+        assert len(inputs.shape) == len(h0.shape)
+
+        s = jnp.exp(self.log_s)
+        states = vmap_to_shape(self.gru, inputs.shape)(inputs / s, h0)
+        states = (states - h0) / s + h0
+        return states
+
+
+class MultiScaleGRU(eqx.Module):
+    nchannel: int
+    nlayer: int
+    encoder: MLP
+    scale_grus: List[List[ScaleGRU]]
+    mlps: List[MLP]
+    classifier: MLP
+
+    def __init__(self, ninp: int, nchannel: int, nstate: int, nlayer: int, nclass: int, key: prng.PRNGKeyArray):
+        self.nchannel = nchannel
+        self.nlayer = nlayer
+
+        assert nstate % nchannel == 0
+        gru_nstate = int(nstate / nchannel)
+
+        # encode inputs (or rather, project) to have nstates in the feature dimension
+        self.encoder = MLP(ninp=ninp, nstate=nstate, nout=nstate, key=key)
+
+        # nlayers of (scale_gru + mlp) pair
+        self.scale_grus = [[
+            ScaleGRU(
+                ninp=nstate,
+                nstate=gru_nstate,
+                scale=10 ** i,
+                key=key
+            ) for i in range(nchannel)] for _ in range(nlayer)
+        ]
+        self.mlps = [
+            MLP(ninp=nstate, nstate=nstate, nout=nstate, key=key) for _ in range(nlayer)
+        ]
+        assert len(self.scale_grus) == nlayer
+        assert len(self.scale_grus[0]) == nchannel
+        assert len(self.mlps) == nlayer
+
+        # project nstates in the feature dimension to nclasses for classification
+        self.classifier = MLP(ninp=nstate, nstate=nstate, nout=nclass, key=key)
+
+    def __call__(self, inputs: jnp.ndarray, h0: List[jnp.ndarray], yinit_guess: List[jnp.ndarray]):
+        # encode (or rather, project) the inputs
+        inputs = self.encoder(inputs)
+
+        for i in range(self.nlayer):
+            x_from_all_channels = []
+            for ch in range(self.nchannel):
+
+                def model_func(carry: jnp.ndarray, inputs: jnp.ndarray, params: Any):
+                    return self.scale_grus[i][ch](inputs, carry)
+
+                x = jax.vmap(seq1d, in_axes=(None, 0, 0, None, 0))(
+                    model_func,
+                    h0[i],
+                    inputs,
+                    None,
+                    yinit_guess[i][ch]
+                )
+                x_from_all_channels.append(x)
+            x = jnp.concatenate(x_from_all_channels, axis=-1)
+            x = self.mlps[i](x)
+            inputs = x
+        return self.mlps[-1](x), x_from_all_channels
+
+
+
+if __name__ == "__main__":
+    # gru = ScaleGRU(2, 10, 2, jax.random.PRNGKey(1))
+    # mlp = MLP(2, 10, 1, jax.random.PRNGKey(1))
+    ninp = 1
+    nstate = 32
+    nchannel = 4
+    nclass = 2
+    nlayer = 3
+    model = MultiScaleGRU(
+        ninp=ninp,
+        nchannel=nchannel,
+        nstate=nstate,
+        nlayer=nlayer,
+        nclass=nclass,
+        key=jax.random.PRNGKey(1)
+    )
+    mlp = MLP(
+        ninp=ninp,
+        nstate=nstate,
+        nout=nstate,
+        key=jax.random.PRNGKey(1)
+    )
+    scale_gru = ScaleGRU(
+        ninp=nstate,
+        nstate=nstate,
+        scale=1,
+        key=jax.random.PRNGKey(1)
+    )
+
+    nseq = 69
+    batch_size = 7
+    carry = jnp.zeros(
+        (batch_size, int(nstate / nchannel))
+    )  # (batch_size, nstates)
+    inputs = jax.random.normal(
+        jax.random.PRNGKey(1),
+        (batch_size, nseq, ninp),
+    )  # (batch_size, nsequence, nstates)
+    yinit_guess = jax.random.normal(
+        jax.random.PRNGKey(1),
+        (batch_size, nseq, int(nstate / nchannel)),
+    )  # (batch_size, nsequence, nstates)
+    # y, _ = scale_gru(mlp(inputs[:, 0, :]), carry)
+    y = model(inputs, [carry for _ in range(nchannel)], [[yinit_guess for _ in range(nchannel)] for _ in range(nlayer)])
+    pdb.set_trace()
+    # jax.config.update('jax_enable_x64', True)
+
+    # model = MultiScaleGRU(
+    #     nchannels=1,
+    #     nstates=32,
+    #     nlayers=3,
+    #     nclasses=2,
+    #     dtype=jnp.float64
+    # )
+
+    # key = jax.random.PRNGKey(0)
+
+    # carry = model.initialize_carry(
+    #     batch_size=24
+    # )  # (batch_size, nstates)
+    # inputs = jax.random.normal(
+    #     key,
+    #     (24, 10, 1),
+    #     dtype=jnp.float64
+    # )  # (batch_size, nsequence, nstates)
+    # yinit_guess = jax.random.normal(
+    #     key,
+    #     (24, 10, 32),
+    #     dtype=jnp.float64
+    # )  # (batch_size, nsequence, nstates)
+    # params = model.init(key, carry, inputs, yinit_guess)
+
+    # # model = jax.vmap(model, in_axes=(0, 0, 0))
+
+    # model.apply(params, carry, inputs, yinit_guess)
