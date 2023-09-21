fc59df8ebc2a746db1bd38bf13e8eea0ef9c3c24
yhl48 <limjohnyh@gmail.com>
2023-09-21 10:20:16 +0000

    cleaned code

--- deer/experiments/deer_rnn/utils.py
+++ deer/experiments/deer_rnn/utils.py
@@ -15,7 +15,6 @@ from dataloaders.md_sine import TimeSeriesDataModule
 from dataloaders.md_eigenworms import EigenWormsDataModule
 from dataloaders.md_ecg200 import ECG200DataModule
 
-
 import pdb
 
 
@@ -25,7 +24,6 @@ def prep_batch(
 ) -> Tuple[jnp.ndarray, jnp.ndarray]:
     assert len(batch) == 2
     x, y = batch
-    # x = jnp.asarray(x.numpy()[:, ::2, :], dtype=dtype)
     x = jnp.asarray(x.numpy(), dtype=dtype)
     y = jnp.asarray(y.numpy())
     return x, y
@@ -44,15 +42,7 @@ def compute_metrics(
     logits: jnp.ndarray,
     labels: jnp.ndarray
 ) -> Dict[str, jnp.ndarray]:
-    # print(logits)
     loss = jnp.mean(optax.softmax_cross_entropy_with_integer_labels(logits, labels))
-    # jax.debug.print("{loss}", loss=loss)
-    # jax.debug.print("{logits}", logits=logits)
-
-    # print_argmax = jax.tree_map(print, jnp.argmax(logits, -1))
-    # jax.debug.print("{classes} {labels}", classes=jnp.argmax(logits, -1), labels=labels)
-    # print(loss)
-    # pdb.set_trace()
     accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
     metrics = {
         'loss': loss,
