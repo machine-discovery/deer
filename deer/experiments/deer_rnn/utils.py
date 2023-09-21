577c5549fe8949ebfa959d222e1587ea97b01aaa
yhl48 <limjohnyh@gmail.com>
2023-09-21 22:14:09 +0000

    removed all datasets and kept only eigenworms

--- deer/experiments/deer_rnn/utils.py
+++ deer/experiments/deer_rnn/utils.py
@@ -6,16 +6,7 @@ import optax
 import torch
 from torch.utils.data import Dataset
 
-from dataloaders.lra_image import CIFAR10DataModule
-from dataloaders.lra_pathfinderx import PathfinderXDataModule
-from dataloaders.lra_listops_var import ListOpsVarDataModule
-from dataloaders.lra_text import IMDBDataModule
-from dataloaders.lra_retrieval import AANDataModule
-from dataloaders.md_sine import TimeSeriesDataModule
 from dataloaders.md_eigenworms import EigenWormsDataModule
-from dataloaders.md_ecg200 import ECG200DataModule
-
-import pdb
 
 
 def prep_batch(
@@ -56,59 +47,9 @@ def get_datamodule(
     batch_size: int
 ) -> Dataset:
     dset = dset.lower()
-    if dset == "pathfinder128":
-        return PathfinderXDataModule(
-            data_dir="/home/yhl48/seq2seq/lra_release/lra_release/pathfinder128/curv_baseline",
-            batch_size=batch_size
-        )
-    elif dset == "pathfinder64":
-        return PathfinderXDataModule(
-            data_dir="/home/yhl48/seq2seq/lra_release/lra_release/pathfinder64/curv_baseline",
-            batch_size=batch_size
-        )
-    elif dset == "pathfinder32":
-        return PathfinderXDataModule(
-            data_dir="/home/yhl48/seq2seq/lra_release/lra_release/pathfinder32/curv_baseline",
-            batch_size=batch_size
-        )
-    elif dset == "cifar10":
-        return CIFAR10DataModule(
-            data_dir="/home/yhl48/seq2seq/data",
-            batch_size=batch_size,
-            grayscale=False
-        )
-    elif dset == "cifar10grayscale":
-        return CIFAR10DataModule(
-            data_dir="/home/yhl48/seq2seq/data",
-            batch_size=batch_size,
-            grayscale=True
-        )
-    elif dset == "listops":
-        return ListOpsVarDataModule(
-            data_dir="/home/yhl48/seq2seq/lra_release/listops-1000",
-            batch_size=batch_size
-        )
-    elif dset == "imdb":
-        return IMDBDataModule(
-            data_dir="/home/yhl48/seq2seq/imdb/IMDB",
-            batch_size=batch_size
-        )
-    elif dset == "aan":
-        return AANDataModule(
-            data_dir="/home/yhl48/seq2seq/lra_release/lra_release/tsv_data",
-            batch_size=batch_size
-        )
-    elif dset == "sanity_check":
-        return TimeSeriesDataModule(
-            batch_size=batch_size,
-            nclass=2,
-            seq_length=1000
-        )
-    elif dset == "eigenworms":
+    if dset == "eigenworms":
         return EigenWormsDataModule(
             batch_size=batch_size,  # nseq = 17984, nclass = 5
         )
-    elif dset == "ecg200":
-        return ECG200DataModule(
-            batch_size=batch_size,  # nseq = 96, nclass = 2
-        )
+    else:
+        return NotImplementedError("only eigenworms dataset is available")
